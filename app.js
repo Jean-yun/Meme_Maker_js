c091109bc2b5097141338c5304e18bb5183b6ead
Jean-yun <yesirchloe@gmail.com>
2024-07-24 10:57:11 +0900

    created color mode change fill - draw function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
-
+const modeBtn = document.getElementById("mode-btn")
 
 canvas.width = 800;
 canvas.height = 800;
@@ -12,8 +12,8 @@ ctx.lineWidth = lineWidth.value;
 ctx.strokeStyle = color.value;
 
 
-
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
 	if (isPainting) {
@@ -51,12 +51,31 @@ function onColorClick(event) {
 	color.value = newColor;
 }
 
+function onModeClick(event) {
+	if (isFilling) {
+		isFilling = false;
+		modeBtn.innerText = "Fill"
+	} else {
+		isFilling = true;
+		modeBtn.innerText = "Draw"
+	}
+}
+
+//Filling the entrie canvas 
+function onCanvasClick() {
+	if (isFilling) {
+	ctx.fillRect(0, 0, 800, 800)}
+}
+
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown)
 canvas.addEventListener("mouseup", cancelPainting)
 //bug solving
 canvas.addEventListener("mouseleave", cancelPainting)
+//Fill, draw mode
+canvas.addEventListener("click", onCanvasClick)
+
 
 //lineWidth change tracker
 lineWidth.addEventListener("change", onLineWidthChange)
@@ -66,3 +85,6 @@ color.addEventListener("change", onColorChange)
 
 //colorOption change tracker
 colorOptions.forEach(option => option.addEventListener("click", onColorClick));
+
+//Mode change 
+modeBtn.addEventListener("click", onModeClick)
