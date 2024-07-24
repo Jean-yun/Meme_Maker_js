a2c344d71b1ed7b92ef675eaf9427072ca15e644
Jean-yun <yesirchloe@gmail.com>
2024-07-24 11:04:34 +0900

    created eraser button

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const modeBtn = document.getElementById("mode-btn")
+const destroyBtn = document.getElementById("destroy-btn")
+const eraseBtn = document.getElementById("eraser-btn")
 
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 ctx.strokeStyle = color.value;
 
@@ -64,9 +69,19 @@ function onModeClick(event) {
 //Filling the entrie canvas 
 function onCanvasClick() {
 	if (isFilling) {
-	ctx.fillRect(0, 0, 800, 800)}
+	ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)}
 }
 
+function onDestroyClick() {
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);	
+}
+
+function onEraserClick() {
+	ctx.strokeStyle = "white";
+	isFilling = false;
+	modeBtn.innerText = "Fill"
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown)
@@ -88,3 +103,9 @@ colorOptions.forEach(option => option.addEventListener("click", onColorClick));
 
 //Mode change 
 modeBtn.addEventListener("click", onModeClick)
+
+//Destroy button
+destroyBtn.addEventListener("click", onDestroyClick)
+
+//Erase button
+eraseBtn.addEventListener("click", onEraserClick)
